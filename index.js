2ee4613017e9b542762a91a0db2e41c6950508b6
mfauzanalg <mfauzanalg@gmail.com>
2020-06-06 01:11:55 +0700

    hancooor

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 const fs = require("fs");
 const udemy = require('./udemy');
 
-const elements = {
-    titleSelector: '.course-card--large--1BVxY > .course-card--main-content--3xEIw > .course-card--course-title--2f7tE',
-    imageSelector: '.course-card--large--1BVxY .course-card--image-wrapper--Sxd90 > .course-card--course-image--2sjYP',
-    descSelector: '.course-card--large--1BVxY > .course-card--main-content--3xEIw > .course-card--course-headline--yIrRk',
-    authorSelector: '.course-card--large--1BVxY > .course-card--main-content--3xEIw > .course-card--instructor-list--lIA4f',
-    ratingSelector: '.course-card--large--1BVxY > .course-card--main-content--3xEIw > .course-card--star-rating-wrapper--wwCqc > .star-rating--star-wrapper--2eczq > .udlite-sr-only',
-    reviewSelector: '.course-card--large--1BVxY > .course-card--main-content--3xEIw > .course-card--star-rating-wrapper--wwCqc > .udlite-sr-only',
-    basePriceSelector: '.course-card--large--1BVxY > div.course-card--main-content--3xEIw > div.price-text--container--Ws-fP.course-card--price-text-container--2sb8G > div.price-text--price-part--Tu6MH.price-text--original-price--2e-F5.course-card--list-price--2AO6G.udlite-text-sm > div > span:nth-child(2) > s > span',
-    discPriceSelector: '.course-card--large--1BVxY > div.course-card--main-content--3xEIw > div.price-text--container--Ws-fP.course-card--price-text-container--2sb8G > div.price-text--price-part--Tu6MH.course-card--discount-price--3TaBk.udlite-heading-md > span:nth-child(2) > span',
-    durationSelector: '.course-card--large--1BVxY > div.course-card--main-content--3xEIw > div.udlite-text-xs.course-card--course-meta-info--1hHb3 > span:nth-child(1)',
-    lectureSelector: '.course-card--large--1BVxY > div.course-card--main-content--3xEIw > div.udlite-text-xs.course-card--course-meta-info--1hHb3 > span:nth-child(2)',
-    levelSelector: '.course-card--large--1BVxY > div.course-card--main-content--3xEIw > div.udlite-text-xs.course-card--course-meta-info--1hHb3 > span:nth-child(3)',
-    urlSelector: '.course-list--container--3zXPS > div > .popover--popover--t3rNO > a',
-};
 
 
 (async () => {
 
     const udemy1 = new udemy();
     const udemy2 = new udemy();
-
-    const initial = await udemy1.init('search/?kw=javascr&p=2&q=javascript&src=sac&persist_locale=&locale=en_US&previous_locale=es_ES', elements);
-    const initial2 = await udemy2.init('search/?kw=javascr&p=3&q=javascript&src=sac&persist_locale=&locale=en_US&previous_locale=es_ES', elements);
+    await udemy1.init('search/?kw=javascr&p=3&q=javascript&src=sac&persist_locale=&locale=en_US&previous_locale=es_ES');
+    await udemy2.init('search/?kw=javascr&p=10&q=react&src=sac&persist_locale=&locale=en_US&previous_locale=es_ES');
     
-    const results = udemy1.getResult(elements, 20);
-    const results2 = udemy2.getResult(elements, 20);
-
+    const results = udemy1.getResult(400);
+    const results2 = udemy2.getResult(400);
 
     Promise.all([results, results2]).then((values) => {
         var merged = [].concat.apply([], values);
--- udemy.js
+++ udemy.js
@@ -6,85 +6,170 @@ const UDEMY_URL = (udemy) => `https://www.udemy.com/courses/${udemy}`;
 class udemyPage {
 
     constructor(){
+        this.elements = null;
         this.browser = null;
         this.page = null;
     }
     
-    
-
-    async init(udemy, elements) {
+    async init(udemy) {
+        this.elements = await this.getSelector();
         this.browser = await puppeteer.launch({ headless: true });
         this.page = await this.browser.newPage();
 
         await this.page.goto(UDEMY_URL(udemy));
 
         await this.page.setViewport({
-            width: 1300,
-            height: 2500,
+            width: 2000,
+            height: 5000,
           });
       
     }
 
-    async parseResult(elements){
+    async parseResult(){
          // wait page to load
-         await this.page.waitForSelector(elements.titleSelector, {timeout: 30000});
-        //  await this.autoScroll(this.page);
+         await this.page.waitForSelector(this.elements.titleSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.imageSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.descSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.authorSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.ratingSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.reviewSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.basePriceSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.discPriceSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.durationSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.lectureSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.levelSelector, {timeout: 30000});
+        //  await this.page.waitForSelector(this.elements.urlSelector, {timeout: 30000});
+         await this.autoScroll(this.page);
+         await this.page.waitFor(3000);
 
-        var extractor = await this.page.evaluate((elements) => {
+        const elements = this.elements;
+
+        let extractor = await this.page.evaluate((elements) => {
             window.scrollBy(0, window.innerHeight);
-            var infoArray = [];
             
+
             // select elements you want to extract
-            var titleNodeList = document.querySelectorAll(elements.titleSelector);
-    
-            // create array of extracted data and save it in an array ob object
-            for (var i = 0; i < titleNodeList.length; i++) {
-              infoArray[i] = {
-                no : i,
-                title: titleNodeList[i].innerText
-              };
+            async function getDoc() {
+                doc = {
+                    titleNodeList : await document.querySelectorAll(elements.titleSelector),
+                    imageNodeList : await document.querySelectorAll(elements.imageSelector),
+                    descNodeList : await document.querySelectorAll(elements.descSelector),
+                    authorNodeList : await document.querySelectorAll(elements.authorSelector),
+                    ratingNodeList : await document.querySelectorAll(elements.ratingSelector),
+                    reviewNodeList : await document.querySelectorAll(elements.reviewSelector),
+                    basePriceNodeList : await document.querySelectorAll(elements.basePriceSelector),
+                    discPriceNodeList : await document.querySelectorAll(elements.discPriceSelector),
+                    durationNodeList : await document.querySelectorAll(elements.durationSelector),
+                    lectureNodeList : await document.querySelectorAll(elements.lectureSelector),
+                    levelNodeList : await document.querySelectorAll(elements.levelSelector), 
+                    urlNodeList : await document.querySelectorAll(elements.urlSelector),
+                }
+                return doc;
             }
-            return infoArray;
+
+            let infoArray = getDoc().then((res) => {
+                for (let i = 0; i < res.titleNodeList.length; i++) {
+                    let infoArray = [];
+                    infoArray[i] = {
+                      no : i,
+                      title: res.titleNodeList[i].innerText,
+                      image: res.imageNodeList[i].getAttribute('src'),
+                      desc: res.descNodeList[i].innerText,
+                      author: res.authorNodeList[i].innerText,
+                      rating: res.ratingNodeList[i].innerText.replace('Rating: res.', '').replace(' out of 5', ''),
+                      review: res.reviewNodeList[i].innerText.replace(' reviews', ''),
+                      basePrice: res.basePriceNodeList[i].innerText,
+                      discPrice: res.discPriceNodeList[i].textContent,
+                      duration: res.durationNodeList[i].textContent,
+                      numOfLecture: res.lectureNodeList[i].textContent,
+                      level: res.levelNodeList[i].textContent,
+                      url: res.urlNodeList[i].getAttribute('href')
+                    };
+                }
+                console.log(infoArray);
+                return infoArray;
+            })
+
+            infoArray.then((res) => res);
+            
         }, elements);
 
+        console.log(extractor);
         return extractor;
     }
 
-    async getResult(elements, nr){
+    async getResult(nr){
         let results = [];
 
         do {
 
-            let newResult = await this.parseResult(elements);
+            let newResult = await this.parseResult();
 
             results = [...results, ...newResult];
             console.log('guuguk');
             console.log(results.length)
             if (results.length < nr){
                 console.log('miaw');
-                let nextPageButton = await this.page.$('#udemy > div.main-content-wrapper > div.main-content > div > div > div.course-directory--container--5ZPhr > div.pagination--container--2wc6Z > a.udlite-btn.udlite-btn-small.udlite-btn-secondary.udlite-heading-sm.udlite-btn-icon.udlite-btn-icon-small.udlite-btn-icon-round.pagination--next--5NrLo');
+                let nextPageButton = await this.page.$('.udlite-btn-icon-small.udlite-btn-icon-round.pagination--next--5NrLo');
                 
                 if (nextPageButton) {
-                    console.log('hell u');
                     nextPageButton.click();
                     console.log('wawa');
-                    await this.page.waitForResponse(response => response.ok())
-                    await this.page.waitFor(3000)
-                } else{
-                    await this.browser.close();
-                    break;
+                    await this.page.waitForSelector(this.elements.titleSelector, {timeout: 30000});
                     
+                } else{
+                    console.log('fauzan');
+                    break;
                 }
             }
-
         } while(results.length < nr);
 
-        
+        await this.browser.close();
         return results.slice(0, nr);
 
     }    
 
-}
+    getSelector(){
+        const mainContainer = `.course-card--large--1BVxY > div.course-card--main-content--3xEIw`
+        const extraInfoSelector = `div.udlite-text-xs.course-card--course-meta-info--1hHb3`
+        const priceSelector = `div.price-text--container--Ws-fP.course-card--price-text-container--2sb8G`
 
+        const elements = {
+            titleSelector: `${mainContainer} > .course-card--course-title--2f7tE`,
+            descSelector: `${mainContainer} > .course-card--course-headline--yIrRk`,
+            authorSelector: `${mainContainer} > .course-card--instructor-list--lIA4f`,
+            levelSelector: `${mainContainer} > ${extraInfoSelector} > span:nth-child(3)`,
+            lectureSelector: `${mainContainer} > ${extraInfoSelector} > span:nth-child(2)`,
+            durationSelector: `${mainContainer} > ${extraInfoSelector} > span:nth-child(1)`,
+            urlSelector: `.course-list--container--3zXPS > div > .popover--popover--t3rNO > a`,
+            reviewSelector: `${mainContainer} > .course-card--star-rating-wrapper--wwCqc > .udlite-sr-only`,
+            imageSelector: `.course-card--large--1BVxY .course-card--image-wrapper--Sxd90 > .course-card--course-image--2sjYP`,
+            ratingSelector: `${mainContainer} > .course-card--star-rating-wrapper--wwCqc > .star-rating--star-wrapper--2eczq > .udlite-sr-only`,
+            discPriceSelector: '.course-card--large--1BVxY > div.course-card--main-content--3xEIw > div.price-text--container--Ws-fP.course-card--price-text-container--2sb8G > div.price-text--price-part--Tu6MH.course-card--discount-price--3TaBk.udlite-heading-md > span:nth-child(2) > span',
+            basePriceSelector: '.course-card--large--1BVxY > div.course-card--main-content--3xEIw > div.price-text--container--Ws-fP.course-card--price-text-container--2sb8G > div.price-text--price-part--Tu6MH.price-text--original-price--2e-F5.course-card--list-price--2AO6G.udlite-text-sm > div > span:nth-child(2) > s > span',
+        };
+        return elements;
+    }
+
+    async autoScroll(page){
+        await page.evaluate(async () => {
+            await new Promise((resolve, reject) => {
+                var totalHeight = 0;
+                var distance = 100;
+                var timer = setInterval(() => {
+                    var scrollHeight = document.body.scrollHeight;
+                    window.scrollBy(0, distance);
+                    totalHeight += distance;
+    
+                    if(totalHeight >= scrollHeight){
+                        clearInterval(timer);
+                        resolve();
+                    }
+                }, 50);
+            });
+        });
+    }
+
+}
 
 module.exports = udemyPage;
